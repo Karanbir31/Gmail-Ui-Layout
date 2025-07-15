0c60775cad07a1e1c62a3cc16dd1ff2c4963a37d
Karanbir Singh <karan0031singh@gmail.com>
2025-07-16 00:04:37 +0530

    setup menu host or menu provider with top action bar for enter email details fragment

--- app/src/main/java/com/example/gmailuilayout/EnterMailDetailsFragment.kt
+++ app/src/main/java/com/example/gmailuilayout/EnterMailDetailsFragment.kt
@@ -1,14 +1,66 @@
 package com.example.gmailuilayout
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import com.example.gmailuilayout.databinding.FragmentEnterMailDetailsBinding
+import com.google.android.material.snackbar.Snackbar
 
 class EnterMailDetailsFragment : Fragment() {
+    private lateinit var binding: FragmentEnterMailDetailsBinding
 
+    private val menuProvider = object : MenuProvider {
+        override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+            menuInflater.inflate(R.menu.enter_mail_details_app_bar_menu, menu)
+        }
+
+        override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+            when (menuItem.itemId) {
+                R.id.sendActionMenu -> {
+
+                    binding.root.showSnackBarWithAction("send button click")
+                    return true
+                }
+
+                R.id.attachmentActionMenu -> {
+
+                    binding.root.showSnackBarWithAction("attachment button clicked")
+
+                    return true
+                }
+
+                R.id.discardActionMenu -> {
+                    binding.root.showSnackBarWithAction("discard button click")
+                    return true
+                }
+
+                R.id.settingActionMenu -> {
+                    binding.root.showSnackBarWithAction("setting button click")
+                    return true
+                }
+
+                R.id.saveDraftActionMenu -> {
+                    binding.root.showSnackBarWithAction("save draft button click")
+
+                    return true
+                }
+
+                else -> {
+                    return false
+                }
+            }
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +69,42 @@ class EnterMailDetailsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_enter_mail_details, container, false)
+    ): View {
+        binding = FragmentEnterMailDetailsBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar)
+
+        val menuHost: MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(
+            menuProvider,
+            viewLifecycleOwner,
+            Lifecycle.State.RESUMED
+        )
+
+        binding.apply {
+
+
+        }
+
+    }
+
+    private fun showSnackBarWithAction(message: String, actionText: String, onAction: () -> Unit) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG)
+            .setAction(actionText) {
+                onAction()
+            }
+            .show()
+    }
+
+    private fun View.showSnackBarWithAction(message: String) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG)
+            .show()
+    }
+
 
 }
